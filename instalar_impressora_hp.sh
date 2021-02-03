7c3d48120e03ea1a55de5d1ebe261047780aaeae
Guilherme Rocha <gleite98@gmail.com>
2021-02-03 15:44:18 -0300

    Adicionando script para instalação de impressoras HP

---
+++ instalar_impressora_hp.sh
@@ -0,0 +1,3 @@
+wget https://nchc.dl.sourceforge.net/project/hplip/hplip/3.20.11/hplip-3.20.11.run
+./hplip-3.20.11.run
+#Use os comandos hp-setup e hp-scan para configurar e escanear documentos, respectivamente.
