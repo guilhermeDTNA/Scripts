a228db990b828500957caf95c8d068c1c2558108
Guilherme Rocha <gleite98@gmail.com>
2021-02-19 14:05:52 -0300

    Adicionando instalação do Insomnia

--- instalacao.sh
+++ instalacao.sh
@@ -6,6 +6,18 @@ sudo apt-get install -y vlc
 
 
 echo "---------------"
+echo "Instalando o Insomnia"
+echo "---------------"
+
+echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+    | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y insomnia
+
+
+echo "---------------"
 echo "Instalando o Virtualbox"
 echo "---------------"
 
