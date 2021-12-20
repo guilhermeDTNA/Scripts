e8556251bae7fd28fe6e0f872286167a61eaf3a4
Guilherme Rocha Leite <45232536+guilhermeDTNA@users.noreply.github.com>
2021-12-20 09:26:57 -0300

    Deletando script

--- instalar_kile.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install latex-beamer
-sudo apt-get install texlive-extra-utils texlive-generic-extra texlive-lang-por
-sudo apt-get install texlive-latex-extra texlive-math-extra texlive-pictures
-sudo apt-get install texlive-plain-extra texlive-publishers texlive-science
